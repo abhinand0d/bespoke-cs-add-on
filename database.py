537c877f1afc678d7c90b4a5eaf952464db1d3fc
Abhinand Dhandapani <abhinandd704@gmail.com>
2023-08-04 11:07:45 +0530

    Update Sales & Item List from db

--- database.py
+++ database.py
@@ -10,4 +10,27 @@ class Bespoke():
         cur.execute("SELECT * FROM salesmast")
         print(cur.fetchall())
 
-Bespoke.sale(date="15")
+    def item(code):
+        cur = conn.cursor()
+        ITEM_CODE = code
+        cur.execute(f"SELECT citname FROM item WHERE citcode='{code}'")
+        ITEM_NAME = cur.fetchone()[0]
+        cur.execute(f"SELECT nsalerate,nprrate FROM itembatch WHERE citbatcode='{code}'")
+        ITEM_SALE_RATE, ITEM_PURCHASE_RATE = cur.fetchone()
+
+        return ITEM_CODE, ITEM_NAME, ITEM_PURCHASE_RATE, ITEM_SALE_RATE
+    
+    def item_list():
+        cur = conn.cursor()
+        ITEM_LIST = []
+        cur.execute("SELECT citcode FROM item ORDER BY citcode ASC;")
+        for i in cur.fetchall():
+            ITEM_LIST.append(i[0])
+
+        return ITEM_LIST
+
+
+# Bespoke.sale(date="15")
+# cp = Bespoke.item(code="P2393")
+cp = Bespoke.item_list()
+print(cp)
--- pythonanywhere.com/app.py
+++ pythonanywhere.com/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello from BESPOKE-CS-ADD-ON"
+
+@app.route("/item-sync",methods=["POST","GET"])
+def item_sync():
+
+    return "200 OKAY"
